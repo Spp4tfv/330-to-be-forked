82cf86b5246c51a5012c5259dcb540e66f30920d
angeleyy <lika.polyakova.2006@mail.ru>
2024-10-14 18:36:53 +0700

    test

--- script_task3.sh
+++ script_task3.sh
@@ -1,4 +1,12 @@
 echo "Назовите проект"
-read name 
+read name
 echo "Укажите директорию создания проекта"
 read dir
+cd $dir/
+mdkir $name
+cd $name
+mkdir src
+cd src
+mkdir scripts styles images
+touch README.md .gitignore
+echo "Создание проекта выполнено"
